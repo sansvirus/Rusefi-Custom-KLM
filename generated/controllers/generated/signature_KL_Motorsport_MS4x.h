fed129af47caeb45ef4decc7efc7f649421c374a
GitHub git update Action <action@github.com>
2025-07-24 00:59:23 +0000

    Custom Auto-generated Configs
    [no ci]

--- generated/controllers/generated/signature_KL_Motorsport_MS4x.h
+++ generated/controllers/generated/signature_KL_Motorsport_MS4x.h
@@ -3,4 +3,4 @@
 //
 
 #define SIGNATURE_HASH 3062727682
-#define TS_SIGNATURE "rusEFI main.2025.07.23.KL_Motorsport_MS4x.3062727682"
+#define TS_SIGNATURE "rusEFI main.2025.07.24.KL_Motorsport_MS4x.3062727682"
