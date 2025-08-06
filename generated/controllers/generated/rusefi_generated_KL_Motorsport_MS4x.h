ffdf7f7b66af890a193ddbd5db2064615ef01b49
GitHub git update Action <action@github.com>
2025-08-06 00:59:35 +0000

    Custom Auto-generated Configs
    [no ci]

--- generated/controllers/generated/rusefi_generated_KL_Motorsport_MS4x.h
+++ generated/controllers/generated/rusefi_generated_KL_Motorsport_MS4x.h
@@ -2057,7 +2057,7 @@
 #define ts_show_wastegate_sensor true
 #define ts_show_wbo_canbus_index true
 #define ts_show_wbo_canbus_set_index true
-#define TS_SIGNATURE "rusEFI main.2025.08.05.KL_Motorsport_MS4x.524968997"
+#define TS_SIGNATURE "rusEFI main.2025.08.06.KL_Motorsport_MS4x.524968997"
 #define TS_SIMULATE_CAN '>'
 #define TS_SIMULATE_CAN_char >
 #define TS_TEST_COMMAND 't'
--- generated/controllers/generated/signature_KL_Motorsport_MS4x.h
+++ generated/controllers/generated/signature_KL_Motorsport_MS4x.h
@@ -3,4 +3,4 @@
 //
 
 #define SIGNATURE_HASH 524968997
-#define TS_SIGNATURE "rusEFI main.2025.08.05.KL_Motorsport_MS4x.524968997"
+#define TS_SIGNATURE "rusEFI main.2025.08.06.KL_Motorsport_MS4x.524968997"
